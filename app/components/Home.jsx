d7762e89169b166a10fa902b20a51ed620a92a30
Susan Flanagan <SusanFlanagan7@gmail.com>
2017-04-23 02:36:50 -0400

    initialize

---
+++ app/components/Home.jsx
@@ -0,0 +1,33 @@
+import { connect } from 'react-redux';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
+
+export default connect(
+  (state) => {
+    return {
+      
+    }
+  }, 
+  (dispatch) => {
+      return {
+          
+      }
+  }
+)(Home)
